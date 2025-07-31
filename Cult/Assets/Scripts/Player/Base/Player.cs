f1696ab1ef2cf31a807a415f7674e82d63e6ee76
Acriid <wilrichgerdus@gmail.com>
2025-07-31 14:22:39 +0200

    Input system tweaks

--- Cult/Assets/Scripts/Player/Base/Player.cs
+++ Cult/Assets/Scripts/Player/Base/Player.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public PlayerStateMachine playerStateMachine;
+    [Header("Oriantation Transform")]
+    [SerializeField] public Transform oriantation;
 }
