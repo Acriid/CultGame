04db3348d84b4ba1e5c324c54c3419026a9a9b46
Acriid <wilrichgerdus@gmail.com>
2025-10-16 15:32:45 +0200

    fixing draw comparison

--- Cult/Assets/Scripts/Rituals/Drawing/DrawComparison.cs
+++ Cult/Assets/Scripts/Rituals/Drawing/DrawComparison.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 
 public static class DrawComparison
 {
-    public static bool MeshSimilar2D(Mesh CompareMesh, MeshCollider StaticMesh, float threshold = 0.5f, float compareratio = 0.85f)
+    public static bool MeshSimilar2D(GameObject CompareObject, MeshCollider StaticMesh, float threshold = 0.5f, float compareratio = 0.85f)
     {
         int insideCount = 0;
+        Mesh CompareMesh = CompareObject.GetComponent<MeshFilter>().mesh;
         Vector3[] compareVertices = CompareMesh.vertices;
-
         foreach (var point in compareVertices)
         {
-            Vector3 worldPoint = StaticMesh.transform.TransformPoint(point);
-            Vector3 closest = StaticMesh.ClosestPoint(worldPoint);
+            Vector3 worldPoint = CompareObject.transform.TransformPoint(point);
+            Vector3 closest = StaticMesh.ClosestPointOnBounds(worldPoint);
             float dist = Vector3.Distance(worldPoint, closest);
-
-            if (dist <= threshold) insideCount++;
+            if (dist <= threshold) { insideCount++; }
         }
-
+        Debug.Log(insideCount);
         float ratio = (float)insideCount / compareVertices.Length;
+        Debug.Log(ratio > compareratio);
         return ratio > compareratio;
     }
 
