e5a3189650d2490d2cd3432b46a43b5a944bb957
Acriid <wilrichgerdus@gmail.com>
2025-10-19 22:22:35 +0200

    Added warehouse and finished ritual

--- Cult/Assets/Scripts/Rituals/RitualBase.cs
+++ Cult/Assets/Scripts/Rituals/RitualBase.cs
@@ -6,19 +6,7 @@ public class RitualBase : MonoBehaviour
     public List<GameObject> ritualObjects = new List<GameObject>();
     public virtual void CheckRitualProgress()
     {
-        bool RitualDone = true;
-        foreach (GameObject RitualObject in ritualObjects)
-        {
-            if (!RitualObject.GetComponent<RitualObjects>().CorrectObject)
-            {
-                RitualDone = false;
-            }
-        }
-
-        if (RitualDone)
-        {
-            FinishRitual();
-        }
+        
     }
 
     public virtual void FinishRitual()
