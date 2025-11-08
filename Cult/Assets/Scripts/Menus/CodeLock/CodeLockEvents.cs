5ce572cff9ffc68b1ce97fee780f101666edd1a0
Acriid <wilrichgerdus@gmail.com>
2025-11-08 13:23:21 +0200

    Fixed CodeInput

--- Cult/Assets/Scripts/Menus/CodeLock/CodeLockEvents.cs
+++ Cult/Assets/Scripts/Menus/CodeLock/CodeLockEvents.cs
@@ -49,6 +49,7 @@ public class CodeLockEvents : MenuEvents, iLocks
         //Todo - Add opening animation/ play sound
         //Temporary code will just make lock disappear
         door.SetActive(false);
+        this.gameObject.SetActive(false);
         Debug.Log("Unlocked");
     }
     public void SolvingAction(GameObject solveObject)
